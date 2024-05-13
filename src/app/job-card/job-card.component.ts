df5fb7ac54b64c7846e65761ea84efbfeaba9ece
romeuu <sergioromagr@gmail.com>
2024-05-13 19:07:11 +0200

    Añadido degradado radial

--- src/app/job-card/job-card.component.ts
+++ src/app/job-card/job-card.component.ts
@@ -19,14 +19,14 @@ export class JobCardComponent implements OnInit {
         "titulo": 'Desarrollador web - Tecalis Software',
         "subtitulo": 'Anteriormente: Desarrollador web junior',
         "texto": "Con experiencia notable en el sector de las telecomunicaciones en España, me centré en la creación y mantenimiento de componentes y flujos críticos, como la firma electrónica, para las principales compañías del sector. Mi enfoque estaba centrado en la optimización de la eficiencia, seguridad y usabilidad de mi código, llegando así a crear componentes nuevos, pero fiables. Además, conseguí aportar mis conocimientos y habilidades para impulsar el lanzamiento de soluciones digitales propietarias.",
-        "tecnologias": ["PHP (Laravel & Symfony)", "Angular", "TypeScript", "JavaScript", "HTML & CSS", "Ionic", "SQL"],
+        "tecnologias": ["PHP (Laravel & Symfony)", "Angular", "TypeScript", "JavaScript", "HTML & CSS", "Ionic", "Docker", "SQL"],
         "link": "https://tecalis.com"
       },
       {
         "fecha": "2023-2024",
         "titulo": 'Desarrollador web - Battume Creative',
         "texto": 'Como desarrollador web, he tenido la oportunidad de liderar la creación y mantenimiento de páginas web para una variedad clientes en diversos sectores. Durante este tiempo, con buenas prácticas SEO, he conseguido un 50% de aumento en tráfico orgánico, además de una mejora significativa en el rendimiento. Cada proyecto, era una oportunidad nueva para impulsar la digitalización de estas empresas.',
-        "tecnologias": ["PHP (Laravel)", "Angular", "WordPress", "HTML & CSS", "TypeScript", "SQL"],
+        "tecnologias": ["PHP (Laravel)", "Angular", "WordPress", "HTML & CSS", "TypeScript", "AWS", "SQL"],
         "link": "https://battume.com"
       }
     ]
