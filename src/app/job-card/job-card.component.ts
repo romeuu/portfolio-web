a8c0a665d628a37110b7fbee2b6b4b2d5588cad9
Sergio Romeu <sergioromagr@gmail.com>
2025-01-17 21:35:48 +0100

    feat: Updated experience at job cards

--- src/app/job-card/job-card.component.ts
+++ src/app/job-card/job-card.component.ts
@@ -15,20 +15,27 @@ export class JobCardComponent implements OnInit {
   ngOnInit(): void {
     this.experiencia = [
       {
-        "fecha": "2020-2023",
+        "fecha": "2024 - Actualmente",
+        "titulo": 'Desarrollador web - AXPE Cantabria',
+        "texto": 'Desde junio, he trabajado como desarrollador fullstack en un proyecto que me ha permitido seguir creciendo profesionalmente. En este rol, he utilizado Java para el desarrollo backend y he continuado ganando experiencia en Angular para el frontend, consolidando mis habilidades en ambas áreas.',
+        "tecnologias": ["Angular", "Java (Spring Boot)", "HTML & CSS", "TypeScript", "SQL"],
+        "link": "https://www.axpe.com/"
+      },
+      {
+        "fecha": "2023 - 2024",
+        "titulo": 'Desarrollador web - Battume Creative',
+        "texto": 'Como desarrollador web, he tenido la oportunidad de liderar la creación y mantenimiento de páginas web para una variedad clientes en diversos sectores. Durante este tiempo, con buenas prácticas SEO, he conseguido un 50% de aumento en tráfico orgánico, además de una mejora significativa en el rendimiento. Cada proyecto, era una oportunidad nueva para impulsar la digitalización de estas empresas.',
+        "tecnologias": ["PHP (Laravel)", "Angular", "WordPress", "HTML & CSS", "TypeScript", "AWS", "SQL"],
+        "link": "https://battume.com"
+      },
+      {
+        "fecha": "2020 - 2023",
         "titulo": 'Desarrollador web - Tecalis Software',
         "subtitulo": 'Anteriormente: Desarrollador web junior',
         "texto": "Con experiencia notable en el sector de las telecomunicaciones en España, me centré en la creación y mantenimiento de componentes y flujos críticos, como la firma electrónica, para las principales compañías del sector. Mi enfoque estaba centrado en la optimización de la eficiencia, seguridad y usabilidad de mi código, llegando así a crear componentes nuevos, pero fiables. Además, conseguí aportar mis conocimientos y habilidades para impulsar el lanzamiento de soluciones digitales propietarias.",
         "tecnologias": ["PHP (Laravel & Symfony)", "Angular", "TypeScript", "JavaScript", "HTML & CSS", "Ionic", "Docker", "SQL"],
         "link": "https://tecalis.com"
       },
-      {
-        "fecha": "2023-2024",
-        "titulo": 'Desarrollador web - Battume Creative',
-        "texto": 'Como desarrollador web, he tenido la oportunidad de liderar la creación y mantenimiento de páginas web para una variedad clientes en diversos sectores. Durante este tiempo, con buenas prácticas SEO, he conseguido un 50% de aumento en tráfico orgánico, además de una mejora significativa en el rendimiento. Cada proyecto, era una oportunidad nueva para impulsar la digitalización de estas empresas.',
-        "tecnologias": ["PHP (Laravel)", "Angular", "WordPress", "HTML & CSS", "TypeScript", "AWS", "SQL"],
-        "link": "https://battume.com"
-      }
     ]
   }
 }
